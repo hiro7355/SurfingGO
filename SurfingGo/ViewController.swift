6fb77a43162ea2380db8d8204c344dc93707f006
nozawa michihiro <nozawa@brabio.jp>
2017-09-26 16:27:10 +0900

    Initial Commit

---
+++ SurfingGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SurfingGo
+//
+//  Created by 野澤 通弘 on 2017/09/26.
+//  Copyright © 2017年 ikaika software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
